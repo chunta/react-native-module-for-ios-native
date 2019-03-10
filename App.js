30eb3d5b881a32527a1173a1b6e972e38e345cc0
Rex <rex.chen@appledaily.com.tw>
2019-03-10 22:48:46 +0800

    date format

--- App.js
+++ App.js
@@ -11,9 +11,10 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import {NativeModules} from 'react-native';
 
+var myDate = new Date();
 var nativeModule = NativeModules.RNHelloWorld;
 nativeModule.addEvent('Birthday Party', 'Rex Chen');
-
+nativeModule.addDateEvent('JORDAN', 'Rex Chen', myDate.getTime());
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
--- ios/RNHelloWorld.m
+++ ios/RNHelloWorld.m
@@ -17,4 +17,12 @@ RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
   RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
 }
 
+RCT_EXPORT_METHOD(addDateEvent:(NSString *)name location:(NSString *)location date:(NSDate *)date)
+{
+  // Date is ready to use!
+  NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+  [formatter setDateFormat:@"YYYY-MM-dd"];
+  RCTLogInfo(@"Pretending to create an event %@ %@", [formatter stringFromDate:date], name);
+}
+
 @end
